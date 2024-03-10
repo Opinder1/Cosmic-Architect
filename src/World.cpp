9f803c8d9bb20609810ee2ae9e064462716ad8dd
Oliver Pinder <op00153@surrey.ac.uk>
2024-03-10 14:55:24 +0000

    Add method to generate a new world that is owned by that node

--- src/World.cpp
+++ src/World.cpp
@@ -93,6 +93,7 @@ namespace voxel_world
 	void WorldNode::SetWorld(const godot::Ref<World>& world)
 	{
 		m_world = world;
+		m_owner = false;
 	}
 
 	godot::Ref<World> WorldNode::GetWorld()
@@ -100,9 +101,17 @@ namespace voxel_world
 		return m_world;
 	}
 
+	void WorldNode::LoadWorld(const godot::String& path)
+	{
+		m_world.instantiate();
+		m_owner = true;
+
+		m_world->set_path(path);
+	}
+
 	void WorldNode::_process(double delta)
 	{
-		if (m_world.is_valid())
+		if (m_owner && m_world.is_valid())
 		{
 			m_world->Progress(delta);
 		}
@@ -112,6 +121,7 @@ namespace voxel_world
 	{
 		godot::ClassDB::bind_method(godot::D_METHOD("set_world", "world"), &WorldNode::SetWorld);
 		godot::ClassDB::bind_method(godot::D_METHOD("get_world"), &WorldNode::GetWorld);
+		godot::ClassDB::bind_method(godot::D_METHOD("load_world", "path"), &WorldNode::LoadWorld);
 		ADD_PROPERTY(godot::PropertyInfo(godot::Variant::OBJECT, "world", godot::PROPERTY_HINT_RESOURCE_TYPE, "World"), "set_world", "get_world");
 	}
 }
