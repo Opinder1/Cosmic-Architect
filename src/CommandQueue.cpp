f39c4937087a7f65b97819026be9dc38dd7e5dd1
Opinder <opinder.13@gmail.com>
2024-03-21 01:39:39 +0000

    Add command queue processor which command queues flush their commands to. Then the processor can be flushed on the main thread.

--- src/CommandQueue.cpp
+++ src/CommandQueue.cpp
@@ -4,29 +4,44 @@
 
 #include <godot_cpp/core/class_db.hpp>
 
+#include <godot_cpp/classes/os.hpp>
+
 namespace voxel_game
 {
 	CommandQueue::CommandQueue()
 	{
-		Flush();
+		m_thread_id = std::this_thread::get_id();
+
+		m_command_offsets.reserve(256);
+		m_command_buffer.reserve(4096);
 	}
 
 	CommandQueue::~CommandQueue()
-	{}
-
-	void CommandQueue::SetObject(uint64_t object_id)
 	{
-		m_object_id = object_id;
+		CommandQueueProcessor::get_singleton()->AddCommands(*this);
 	}
 
-	uint64_t CommandQueue::GetObject()
+	godot::Ref<CommandQueue> CommandQueue::MakeQueue(const godot::Variant& object)
 	{
-		return m_object_id;
+		if (object.get_type() != godot::Variant::OBJECT)
+		{
+			return nullptr;
+		}
+
+		godot::Ref<CommandQueue> command_queue;
+
+		command_queue.instantiate();
+
+		command_queue->m_object_id = object.operator godot::ObjectID();
+
+		return command_queue;
 	}
 
 	void CommandQueue::Flush()
 	{
-		// Swap(m_command_buffer);
+		DEBUG_ASSERT(std::this_thread::get_id() == m_thread_id, "Should be run by the owning thread");
+
+		CommandQueueProcessor::get_singleton()->AddCommands(*this);
 
 		m_command_offsets.reserve(256);
 		m_command_buffer.reserve(4096);
@@ -48,6 +63,9 @@ namespace voxel_game
 
 	void CommandQueue::_register_command(const godot::StringName& command, const godot::Variant** args, size_t argcount)
 	{
+		DEBUG_ASSERT(std::this_thread::get_id() == m_thread_id, "Should be run by the owning thread");
+		DEBUG_ASSERT(argcount <= Command::k_max_args, godot::vformat("The max args that is supported for commands is %d", Command::k_max_args));
+
 		m_command_offsets.push_back(m_command_buffer.size());
 
 		uint32_t command_offset = m_command_offsets.back();
@@ -57,8 +75,9 @@ namespace voxel_game
 
 		uint8_t* buffer_pos = &m_command_buffer[command_offset];
 
-		godot::StringName* command_ptr = memnew_placement(buffer_pos, godot::StringName);
-		*command_ptr = command;
+		Command* command_ptr = memnew_placement(buffer_pos, Command);
+		command_ptr->command = command;
+		command_ptr->argcount = argcount;
 		buffer_pos += sizeof(godot::StringName);
 
 		for (size_t arg = 0; arg < argcount; arg++)
@@ -72,14 +91,97 @@ namespace voxel_game
 	void CommandQueue::_bind_methods()
 	{
 		godot::ClassDB::bind_vararg_method(godot::METHOD_FLAGS_DEFAULT, "register_command", &CommandQueue::_register_command_vararg, godot::MethodInfo("register_command", godot::PropertyInfo(godot::Variant::STRING, "command")));
-		godot::ClassDB::bind_method(godot::D_METHOD("set_object", "object_id"), &CommandQueue::SetObject);
-		godot::ClassDB::bind_method(godot::D_METHOD("get_object"), &CommandQueue::GetObject);
+		godot::ClassDB::bind_static_method(get_class_static(), godot::D_METHOD("make_queue", "object"), &CommandQueue::MakeQueue);
 
 		ADD_PROPERTY(godot::PropertyInfo(godot::Variant::INT, "object", godot::PROPERTY_HINT_OBJECT_ID), "set_object", "get_object");
 	}
 
-	void CommandQueue::_cleanup_methods()
-	{
+	std::unique_ptr<CommandQueueProcessor> CommandQueueProcessor::k_singleton;
 
+	CommandQueueProcessor* CommandQueueProcessor::get_singleton()
+	{
+		DEBUG_ASSERT(k_singleton != nullptr, "The singleton doesn't exist");
+		return k_singleton.get();
+	}
+
+	CommandQueueProcessor::CommandQueueProcessor()
+	{}
+
+	CommandQueueProcessor::~CommandQueueProcessor()
+	{}
+
+	void CommandQueueProcessor::AddCommands(CommandQueue& command_queue)
+	{
+		std::lock_guard lock(m_mutex);
+
+		Commands& commands = m_command_buffers.emplace_back();
+
+		commands.object_id = command_queue.m_object_id;
+		commands.command_buffer.swap(command_queue.m_command_buffer);
+		commands.command_offsets.swap(command_queue.m_command_offsets);
+	}
+
+	void CommandQueueProcessor::ProcessCommands(const Commands& commands)
+	{
+		godot::Variant object = godot::ObjectDB::get_instance(commands.object_id);
+
+		for (uint32_t command_offset : commands.command_offsets)
+		{
+			DEBUG_ASSERT(commands.command_buffer.size() > command_offset, "Command offset submitted is outside of the range of the buffer");
+
+			const uint8_t* buffer_pos = &commands.command_buffer[command_offset];
+
+			const Command* command_ptr = reinterpret_cast<const Command*>(buffer_pos);
+
+			DEBUG_ASSERT(commands.command_buffer.size() > command_offset + sizeof(Command) + (sizeof(godot::Variant) * command_ptr->argcount), "Command buffer doesn't fit the command and its arguments");
+
+			const godot::Variant* args = reinterpret_cast<const godot::Variant*>(buffer_pos + sizeof(Command));
+
+			const godot::Variant* argptrs[Command::k_max_args];
+			if (command_ptr->argcount > 0)
+			{
+				for (size_t arg = 0; arg < command_ptr->argcount; arg++)
+				{
+					argptrs[arg] = &args[arg];
+				}
+			}
+
+			godot::Variant ret;
+			GDExtensionCallError error;
+			object.callp(command_ptr->command, argptrs, command_ptr->argcount, ret, error);
+
+			command_ptr->~Command();
+
+			for (size_t arg = 0; arg < command_ptr->argcount; arg++)
+			{
+				args[arg].~Variant();
+			}
+		}
+	}
+
+	void CommandQueueProcessor::Flush()
+	{
+		DEBUG_ASSERT(godot::OS::get_singleton()->get_thread_caller_id() == godot::OS::get_singleton()->get_main_thread_id(), "The processor should only be flushed on the main thread");
+
+		std::lock_guard lock(m_mutex);
+
+		for (Commands& commands : m_command_buffers)
+		{
+			ProcessCommands(commands);
+		}
+
+		m_command_buffers.clear();
+	}
+
+	void CommandQueueProcessor::_bind_methods()
+	{
+		godot::ClassDB::bind_method(godot::D_METHOD("flush"), &CommandQueueProcessor::Flush);
+
+		k_singleton = std::make_unique<CommandQueueProcessor>();
+	}
+
+	void CommandQueueProcessor::_cleanup_methods()
+	{
+		k_singleton.reset();
 	}
 }
--- src/CommandQueue.h
+++ src/CommandQueue.h
@@ -1,24 +1,39 @@
 #pragma once
 
 #include <godot_cpp/classes/ref_counted.hpp>
+#include <godot_cpp/classes/ref.hpp>
+
+#include <godot_cpp/variant/string_name.hpp>
+#include <godot_cpp/variant/variant.hpp>
 
 #include <godot_cpp/templates/local_vector.hpp>
 
+#include <memory>
 #include <deque>
+#include <thread>
+#include <mutex>
 
 namespace voxel_game
 {
+	struct Command
+	{
+		constexpr static size_t k_max_args = 16;
+
+		godot::StringName command;
+		size_t argcount;
+	};
+
 	class CommandQueue : public godot::RefCounted
 	{
 		GDCLASS(CommandQueue, godot::RefCounted);
+
+		friend class CommandQueueProcessor;
 
 	public:
 		CommandQueue();
 		~CommandQueue();
 
-		void SetObject(uint64_t object_id);
-
-		uint64_t GetObject();
+		static godot::Ref<CommandQueue> MakeQueue(const godot::Variant& object);
 
 		template<class... Args>
 		void RegisterCommand(const godot::StringName& command, Args... args)
@@ -31,6 +46,45 @@ namespace voxel_game
 			return _register_command(command, sizeof...(args) == 0 ? nullptr : (const godot::Variant**)argptrs, sizeof...(args));
 		}
 
+		void Flush();
+
+	public:
+		static void _bind_methods();
+
+	private:
+		void _register_command_vararg(const godot::Variant** p_args, GDExtensionInt p_argcount, GDExtensionCallError& error);
+		void _register_command(const godot::StringName& command, const godot::Variant** args, size_t argcount);
+
+	private:
+		std::thread::id m_thread_id;
+
+		godot::ObjectID m_object_id;
+
+		std::vector<uint8_t> m_command_buffer;
+		std::vector<uint32_t> m_command_offsets;
+	};
+
+	class CommandQueueProcessor : public godot::Object
+	{
+		GDCLASS(CommandQueueProcessor, godot::Object);
+
+		struct Commands
+		{
+			godot::ObjectID object_id;
+
+			std::vector<uint8_t> command_buffer;
+			std::vector<uint32_t> command_offsets;
+		};
+
+	public:
+		static CommandQueueProcessor* get_singleton();
+
+		CommandQueueProcessor();
+		~CommandQueueProcessor();
+
+		void AddCommands(CommandQueue& command_queue);
+
+		void ProcessCommands(const Commands& commands);
 
 		void Flush();
 
@@ -39,13 +93,9 @@ namespace voxel_game
 		static void _cleanup_methods();
 
 	private:
-		void _register_command_vararg(const godot::Variant** p_args, GDExtensionInt p_argcount, GDExtensionCallError& error);
-		void _register_command(const godot::StringName& command, const godot::Variant** args, size_t argcount);
+		static std::unique_ptr<CommandQueueProcessor> k_singleton;
 
-	private:
-		godot::ObjectID m_object_id;
-
-		std::vector<uint8_t> m_command_buffer;
-		std::vector<uint32_t> m_command_offsets;
+		std::mutex m_mutex;
+		std::vector<Commands> m_command_buffers;
 	};
 }
