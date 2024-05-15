cd34b0e8adcec6284ce7e8a7def44ee03dbd9048
Opinder <opinder.13@gmail.com>
2024-05-16 00:23:31 +0100

    Add contiguous rid owner class where all the objects are stored in a contiguous array

---
+++ src/Util/ContiguousRIDOwner.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <godot_cpp/variant/rid.hpp>
+#include <godot_cpp/templates/rid_owner.hpp>
+#include <godot_cpp/variant/packed_byte_array.hpp>
+
+#include <memory>
+#include <algorithm>
+
+namespace voxel_game
+{
+	template<class T>
+	class RIDContiguousOwner
+	{
+	public:
+		RIDContiguousOwner()
+		{
+			m_buffer.resize(sizeof(T) * 8);
+		}
+
+		godot::RID Add(const T& input)
+		{
+			uint64_t offset = m_buffer.size();
+			m_buffer.resize(offset + sizeof(T));
+
+			T* item = reinterpret_cast<T*>(m_buffer.ptrw() + offset);
+
+			*item = input;
+
+			return m_item_offsets.make_rid(item);
+		}
+
+		T* Get(godot::RID id)
+		{
+			return m_item_offsets.get_or_null(id);
+		}
+
+		void Remove(godot::RID id)
+		{
+			T* target_item = m_item_offsets.get_or_null(id);
+
+			if (target_item == nullptr)
+			{
+				return;
+			}
+
+			uint32_t count = m_item_offsets.get_rid_count();
+			std::unique_ptr<godot::RID[]> rids = std::make_unique<godot::RID[]>(count);
+
+			m_item_offsets.fill_owned_buffer(rids.get());
+
+			godot::RID* end = rids.get() + count;
+			for (godot::RID* rid = rids.get(); rid < end; rid++)
+			{
+				T* item = m_item_offsets.get_or_null(rid);
+
+				if (item > target_item)
+				{
+					m_item_offsets.replace(rid, item - 1);
+				}
+			}
+
+			m_item_offsets.free(id);
+
+			T* end = reinterpret_cast<T*>(m_buffer.ptrw() + m_buffer.count());
+
+			std::copy(target_item, end, target_item - 1);
+		}
+
+		const godot::PackedByteArray& GetBuffer()
+		{
+			return m_buffer;
+		}
+
+	private:
+		godot::RID_PtrOwner<T> m_item_offsets;
+		godot::PackedByteArray m_buffer;
+	};
+}
