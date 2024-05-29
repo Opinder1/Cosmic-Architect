f0473815295b1346c6b3f4e724efe96612d32325
Opinder <opinder.13@gmail.com>
2024-05-29 02:46:24 +0100

    Add a new property macro for RID

--- src/Util/PropertyMacros.h
+++ src/Util/PropertyMacros.h
@@ -4,8 +4,11 @@
 #include <godot_cpp/variant/string.hpp>
 #include <godot_cpp/variant/string_name.hpp>
 
+// Macro shortcuts for creating more complex property types
+
 godot::PropertyInfo MakeObjectProperty(const godot::StringName& name, const godot::String& object_string);
 godot::PropertyInfo MakeEnumProperty(const godot::StringName& name, const godot::String& enum_string);
 
+#define RID_PROPERTY(name) godot::PropertyInfo(godot::Variant::RID, name)
 #define OBJECT_PROPERTY(name, object_) MakeObjectProperty(name, #object_)
 #define ENUM_PROPERTY(name, enum_) MakeEnumProperty(name, #enum_)
