4a800e99b20b2632a68c0c39e59570b6e51dc205
Opinder <opinder.13@gmail.com>
2024-05-29 02:46:49 +0100

    Add getters so that we can then add the scenarios as properties

--- src/UniverseRenderInfo.cpp
+++ src/UniverseRenderInfo.cpp
@@ -1,5 +1,7 @@
 #include "UniverseRenderInfo.h"
 
+#include "Util/PropertyMacros.h"
+
 #include <godot_cpp/core/class_db.hpp>
 
 namespace voxel_game
@@ -44,5 +46,12 @@ namespace voxel_game
 		godot::ClassDB::bind_method(godot::D_METHOD("set_galaxy_scenario", "scenario"), &UniverseRenderInfo::SetGalaxyScenario);
 		godot::ClassDB::bind_method(godot::D_METHOD("set_star_scenario", "scenario"), &UniverseRenderInfo::SetStarScenario);
 		godot::ClassDB::bind_method(godot::D_METHOD("set_main_scenario", "scenario"), &UniverseRenderInfo::SetMainScenario);
+		godot::ClassDB::bind_method(godot::D_METHOD("get_galaxy_scenario"), &UniverseRenderInfo::GetGalaxyScenario);
+		godot::ClassDB::bind_method(godot::D_METHOD("get_star_scenario"), &UniverseRenderInfo::GetStarScenario);
+		godot::ClassDB::bind_method(godot::D_METHOD("get_main_scenario"), &UniverseRenderInfo::GetMainScenario);
+
+		ADD_PROPERTY(RID_PROPERTY("galaxy_scenario"), "set_galaxy_scenario", "get_galaxy_scenario");
+		ADD_PROPERTY(RID_PROPERTY("star_scenario"), "set_star_scenario", "get_star_scenario");
+		ADD_PROPERTY(RID_PROPERTY("main_scenario"), "set_main_scenario", "get_main_scenario");
 	}
 }
