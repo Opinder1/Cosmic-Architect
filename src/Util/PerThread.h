f034dca22a3b782e184b5ca92197539e1d567770
Opinder <opinder.13@gmail.com>
2024-07-08 22:07:26 +0100

    Add comment for this class

--- src/Util/PerThread.h
+++ src/Util/PerThread.h
@@ -8,5 +8,6 @@ constexpr static const size_t k_worker_thread_max = 16;
 template<class DataT>
 struct alignas(std::hardware_destructive_interference_size) AlignedData : DataT {};
 
+// An array to store data for worker threads that avoids false sharing
 template<class DataT>
 using PerThread = std::array<AlignedData<DataT>, k_worker_thread_max>;
