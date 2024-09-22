f65d89b0870308116fe069ed6d4f13bb2cf1b378
Opinder <opinder.13@gmail.com>
2024-09-22 12:25:48 +0100

    Add new entity loader class

---
+++ src/Simulation/EntityLoader.cpp
@@ -0,0 +1,87 @@
+#include "EntityLoader.h"
+
+namespace sim
+{
+	const size_t k_entity_pool_max = 1024;
+
+	EntityLoader::EntityLoader(const flecs::world& world)
+	{
+		m_world = world;
+
+		m_read_stage = world.async_stage();
+		m_swap_stage = world.async_stage();
+		m_write_stage = world.async_stage();
+
+		m_running.store(true, std::memory_order_release);
+		m_thread = std::thread(&EntityLoader::ThreadLoop, this);
+	}
+
+	EntityLoader::~EntityLoader()
+	{
+		m_running.store(false, std::memory_order_release);
+
+		if (m_thread.joinable())
+		{
+			m_thread.join();
+		}
+	}
+
+	void EntityLoader::Progress()
+	{
+		if (m_modifications_ready.load(std::memory_order_acquire))
+		{
+			std::swap(m_read_stage, m_swap_stage);
+
+			m_modifications_ready.store(false, std::memory_order_release);
+		}
+
+		m_read_stage.merge();
+
+		{
+			std::lock_guard lock(m_command_mutex);
+			m_command_read = std::move(m_command_write);
+		}
+
+		while (m_entity_pool.size() < k_entity_pool_max)
+		{
+			m_entity_pool.push_back(m_world.entity());
+		}
+	}
+
+	void EntityLoader::ThreadLoop()
+	{
+		while (m_running.load(std::memory_order_acquire))
+		{
+			std::vector<Command> command_read;
+
+			{
+				std::lock_guard lock(m_command_mutex);
+				command_read = std::move(m_command_read);
+			}
+
+			m_modifications_added = false;
+
+			for (const Command& command : command_read)
+			{
+				switch (command.type)
+				{
+				case CommandType::LoadEntity:
+					LoadEntity(command.uuid);
+					break;
+				}
+			}
+
+			if (m_modifications_added && !m_modifications_ready.load(std::memory_order_acquire))
+			{
+				std::swap(m_write_stage, m_swap_stage);
+
+				m_modifications_ready.store(true, std::memory_order_release);
+			}
+		}
+	}
+
+	void EntityLoader::LoadEntity(UUID id)
+	{
+
+	}
+}
---
+++ src/Simulation/EntityLoader.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "Util/UUID.h"
+#include "Util/PerThread.h"
+
+#include <robin_hood/robin_hood.h>
+
+#include <flecs/flecs.h>
+
+#include <TKRZW/tkrzw_thread_util.h>
+
+#include <thread>
+
+namespace sim
+{
+	class EntityLoader
+	{
+	private:
+		enum class CommandType
+		{
+			LoadEntity,
+			SaveEntity,
+			InstanceEntity,
+			UnloadEntity,
+		};
+
+		struct Command
+		{
+			CommandType type;
+
+			union // Command data
+			{
+				flecs::entity_t entity;
+				UUID uuid;
+			};
+		};
+
+	public:
+		EntityLoader(const flecs::world& world);
+		~EntityLoader();
+
+		void Progress();
+
+	private:
+		void ThreadLoop();
+
+		void LoadEntity(UUID id);
+
+	private:
+		std::thread m_thread;
+		std::atomic_bool m_running = false;
+
+		// Commands requested to loader
+		tkrzw::SpinMutex m_command_mutex;
+		alignas(k_cache_line) std::vector<Command> m_command_write;
+		alignas(k_cache_line) std::vector<Command> m_command_read;
+
+		// Modifications output by loader
+		struct alignas(k_cache_line)
+		{
+			std::atomic_bool m_modifications_ready = false;
+			flecs::world m_write_stage;
+			flecs::world m_swap_stage;
+			flecs::world m_read_stage;
+		};
+
+		// Flag set when modifcations are made
+		bool m_modifications_added = false;
+
+		// Alive entity handles that can be used in the stages and world reference to generate more
+		flecs::world m_world;
+		std::vector<flecs::entity_t> m_entity_pool;
+
+		// Cache of already loaded entities
+		robin_hood::unordered_map<UUID, flecs::entity_t, UUIDHash> m_entity_cache;
+	};
+}
