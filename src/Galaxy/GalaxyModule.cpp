Opinder1/Cosmic-Architect

commit d34ad44006ddd0e8720d3a26f856a6a050fa467f

Massive refactor of whole spatial world system. Also added naming scheme for components where components start with C, relationships start with R and phases start with P.
The project currently is not tested and should crash when running this version.

src/Galaxy/GalaxyModule.cpp
@@ -30,22 +30,22 @@ namespace voxel_game::galaxy
 		galaxy_entity.set_name("SimulatedGalaxy");
 #endif
 
-		galaxy_entity.emplace<sim::Path>(path);
+		galaxy_entity.emplace<sim::CPath>(path);
 
-		loading::Database& database = galaxy_entity.ensure<loading::Database>();
+		loading::CDatabase& database = galaxy_entity.ensure<loading::CDatabase>();
 		database.path = path.path_join("entities.db");
 
 		galaxy_entity.child_of(universe_entity);
 
-		galaxy_entity.add<World>();
+		galaxy_entity.add<CWorld>();
 
-		spatial3d::WorldMarker& spatial_world = galaxy_entity.ensure<spatial3d::WorldMarker>();
-		spatial_world.world.max_scale = spatial3d::k_max_world_scale;
+		spatial3d::CWorld& spatial_world = galaxy_entity.ensure<spatial3d::CWorld>();
+		spatial_world.world->max_scale = spatial3d::k_max_world_scale;
 
-		galaxy_entity.add<physics3d::Position>();
-		galaxy_entity.add<physics3d::Rotation>();
+		galaxy_entity.add<physics3d::CPosition>();
+		galaxy_entity.add<physics3d::CRotation>();
 
-		spatial3d::InitializeWorldScales(galaxy_entity, spatial_world);
+		spatial3d::InitializeWorldScaleEntities(galaxy_entity, *spatial_world.world);
 
 		// We want the simulated galaxy to load all galaxies around it
 		spatial3d::Loader& spatial_loader = galaxy_entity.ensure<spatial3d::Loader>();
@@ -53,7 +53,7 @@ namespace voxel_game::galaxy
 		spatial_loader.min_lod = 0;
 		spatial_loader.max_lod = spatial3d::k_max_world_scale;
 
-		galaxy_entity.add<rendering::Transform>();
+		galaxy_entity.add<rendering::CTransform>();
 
 		return galaxy_entity;
 	}
@@ -67,13 +67,17 @@ namespace voxel_game::galaxy
 		world.import<spatial3d::Components>();
 		world.import<physics3d::Components>();
 
+		spatial3d::NodeType::RegisterType<Node>();
+		spatial3d::ScaleType::RegisterType<Scale>();
+
 		// Initialise the spatial world of a galaxy
-		world.observer<World, spatial3d::WorldMarker>(DEBUG_ONLY("GalaxyInitializeSpatialWorld"))
+		world.observer<spatial3d::CWorld>(DEBUG_ONLY("GalaxyInitializeSpatialWorld"))
 			.event(flecs::OnAdd)
-			.each([](World& galaxy_world, spatial3d::WorldMarker& spatial_world)
+			.with<const CWorld>()
+			.each([](spatial3d::CWorld& spatial_world)
 		{
-			galaxy_world.node_entry = spatial_world.world.node_type.AddEntry<Node>();
-			galaxy_world.scale_entry = spatial_world.world.scale_type.AddEntry<Scale>();
+			spatial_world.types.node_type.AddType<Node>();
+			spatial_world.types.scale_type.AddType<Scale>();
 		});
 	}
 }

commit 760ea3da69bb3c1aa536a00afc6f20abf7d48da2

Fix bugs introduced in d34ad440

src/Galaxy/GalaxyModule.cpp
@@ -40,7 +40,10 @@ namespace voxel_game::galaxy
 		galaxy_entity.add<CWorld>();
 
 		spatial3d::CWorld& spatial_world = galaxy_entity.ensure<spatial3d::CWorld>();
-		spatial_world.world->max_scale = spatial3d::k_max_world_scale;
+
+		DEBUG_ASSERT(!world.is_deferred(), "Observers need to be invoked to initialize poly types");
+
+		spatial_world.world = spatial3d::CreateWorld(spatial_world.types, spatial3d::k_max_world_scale);
 
 		galaxy_entity.add<physics3d::CPosition>();
 		galaxy_entity.add<physics3d::CRotation>();

src/Render/RenderModule.cpp
@@ -35,7 +35,9 @@ namespace voxel_game::rendering
         // Make sure we initially get some rids
         AllocatorServer::get_singleton()->RequestRIDs(true);
 
-        world.add<CContext>();
+        CContext& context = world.ensure<CContext>();
+
+        thread_context = &context.threads[0];
 
         // Flush each threads render commands to the command queue server which will run them on the rendering server thread
         world.system<CContext>(DEBUG_ONLY("FrameFlushCommands"))
@@ -93,10 +95,6 @@ namespace voxel_game::rendering
         // Update the render tree nodes transform based on the current nodes position, rotation, scale and parents transform
         world.system<CTransform, const physics3d::CPosition*, const physics3d::CRotation*, const physics3d::CScale*, const CTransform*>(DEBUG_ONLY("UpdateTreeNodeTransforms"))
             .multi_threaded()
-            .term_at(0)
-            .term_at(1)
-            .term_at(2)
-            .term_at(3)
             .term_at(4).cascade(flecs::ChildOf)
             .each([](CTransform& transform, const physics3d::CPosition* position, const physics3d::CRotation* rotation, const physics3d::CScale* scale, const CTransform* parent_transform)
         {
@@ -131,7 +129,6 @@ namespace voxel_game::rendering
             .multi_threaded()
             .term_at(0).second(flecs::Any)
             .term_at(1).self().up(flecs::ChildOf)
-            .term_at(2).singleton()
             .each([](CInstance& instance, const CTransform& transform)
         {
             DEBUG_ASSERT(instance.id != godot::RID(), "Instance should be valid");
@@ -148,8 +145,6 @@ namespace voxel_game::rendering
         world.observer<CScenario>(DEBUG_ONLY("AddScenario"))
             .event(flecs::OnAdd)
             .yield_existing()
-            .term_at(0)
-            .term_at(1).singleton().filter()
             .with<const COwnedScenario>()
             .each([](flecs::entity entity, CScenario& scenario)
         {
@@ -162,14 +157,12 @@ namespace voxel_game::rendering
 
         world.observer<const CScenario>(DEBUG_ONLY("RemoveScenario"))
             .event(flecs::OnRemove)
-            .term_at(0)
-            .term_at(1).singleton().filter()
             .with<const COwnedScenario>()
             .each([](const CScenario& scenario)
         {
             DEBUG_ASSERT(scenario.id != godot::RID(), "Scenario should be valid");
 
-            GetContext().commands.AddCommand<&godot::RenderingServer::free_rid>(scenario.id);
+            ADD_RENDER_CMD(free_rid, scenario.id);
         });
 
         world.observer<const CInstance, const CScenario>(DEBUG_ONLY("InstanceSetScenario"))
@@ -177,7 +170,6 @@ namespace voxel_game::rendering
             .yield_existing()
             .term_at(0).second(flecs::Any)
             .term_at(1).up(flecs::ChildOf).filter()
-            .term_at(2).singleton().filter()
             .each([](const CInstance& instance, const CScenario& scenario)
         {
             DEBUG_ASSERT(instance.id != godot::RID(), "Instance should be valid");
@@ -194,12 +186,11 @@ namespace voxel_game::rendering
             .event(flecs::OnAdd)
             .yield_existing()
             .term_at(0).second(flecs::Any)
-            .term_at(1).singleton().filter()
             .each([](flecs::iter& it, size_t i, CInstance& instance)
         {
             EASY_BLOCK("AddUniqueInstance");
 
-            instance.id = GetContext().allocator.GetRID(AllocateType::Instance);
+            instance.id = ALLOC_RENDER_RID(Instance);
 
             flecs::entity entity = it.entity(i);
 
@@ -210,7 +201,6 @@ namespace voxel_game::rendering
         world.observer<const CInstance>(DEBUG_ONLY("RemoveUniqueInstance"))
             .event(flecs::OnRemove)
             .term_at(0).second(flecs::Any)
-            .term_at(1).singleton().filter()
             .each([](const CInstance& instance)
         {
             DEBUG_ASSERT(instance.id != godot::RID(), "Instance should be valid");
@@ -226,7 +216,6 @@ namespace voxel_game::rendering
             .yield_existing()
             .term_at(0).second("$Base")
             .term_at(1).src("$Base").filter()
-            .term_at(2).singleton().filter()
             .each([](const CInstance& instance, const CBase& base)
         {
             DEBUG_ASSERT(instance.id != godot::RID(), "Instance should be valid");
@@ -238,7 +227,6 @@ namespace voxel_game::rendering
         world.observer<const CBase>(DEBUG_ONLY("RemoveBase"))
             .event(flecs::OnRemove)
             .term_at(0)
-            .term_at(1).singleton().filter()
             .each([](const CBase& base)
         {
             DEBUG_ASSERT(base.id != godot::RID(), "Base should be valid");
@@ -255,9 +243,6 @@ namespace voxel_game::rendering
         world.observer<const CPlaceholderCube, CBase>(DEBUG_ONLY("AddPlaceholderCube"))
             .event(flecs::OnAdd)
             .yield_existing()
-            .term_at(0)
-            .term_at(1)
-            .term_at(2).singleton().filter()
             .each([](flecs::entity entity, const CPlaceholderCube& mesh, CBase& base)
         {
             EASY_BLOCK("AddPlaceholderCube");
@@ -273,9 +258,6 @@ namespace voxel_game::rendering
         world.observer<const CMesh, CBase>(DEBUG_ONLY("AddMesh"))
             .event(flecs::OnAdd)
             .yield_existing()
-            .term_at(0)
-            .term_at(1)
-            .term_at(2).singleton().filter()
             .each([](flecs::entity entity, const CMesh& mesh, CBase& base)
         {
             EASY_BLOCK("AddMesh");

src/Render/RenderModule.h
@@ -27,3 +27,5 @@ namespace voxel_game::rendering
 }
 
 #define ADD_RENDER_CMD(command, ...) rendering::GetContext().commands.AddCommand<&godot::RenderingServer::command>(__VA_ARGS__)
+
+#define ALLOC_RENDER_RID(type) rendering::GetContext().allocator.GetRID(rendering::AllocateType::type)

src/Simulation/SimulationComponents.cpp
@@ -14,6 +14,22 @@ namespace voxel_game::sim
 		world.component<CConfig>();
 		world.component<CThreadWorker>();
 		world.component<CEntityPools>();
+
+		world.component<CEntityPools>()
+			.on_add([world = world.c_ptr()](CEntityPools& pools)
+		{
+			for (ThreadEntityPool& pool : pools.threads)
+			{
+				pool.SetWorld(world);
+			}
+		})
+			.on_remove([world = world.c_ptr()](CEntityPools& pools)
+		{
+			for (ThreadEntityPool& pool : pools.threads)
+			{
+				pool.ClearEntities();
+			}
+		});
 	}
 
 	ThreadEntityPool::ThreadEntityPool()

src/Simulation/SimulationModule.cpp
@@ -34,22 +34,6 @@ namespace voxel_game::sim
 			world_time.frame_start_time = Clock::now();
 		});
 
-		world.component<CEntityPools>()
-			.on_add([world = world.c_ptr()](CEntityPools& pools)
-		{
-			for (ThreadEntityPool& pool : pools.threads)
-			{
-				pool.SetWorld(world);
-			}
-		})
-			.on_remove([world = world.c_ptr()](CEntityPools& pools)
-		{
-			for (ThreadEntityPool& pool : pools.threads)
-			{
-				pool.ClearEntities();
-			}
-		});
-
 		world.system<CEntityPools>("ProcessEntityThreadCommands")
 			.kind(flecs::OnUpdate)
 			.term_at(0).src<CEntityPools>()

src/Spatial3D/SpatialComponents.cpp
@@ -8,6 +8,9 @@ namespace voxel_game::spatial3d
 
 		// Components
 		world.component<CLoader>();
+		world.component<CRegion>();
+		world.component<CEntity>();
+		world.component<CScale>();
 		world.component<CWorld>();
 
 		world.component<PNodeCreate>();
@@ -17,5 +20,11 @@ namespace voxel_game::spatial3d
 
 		world.component<EWorldCreate>();
 		world.component<EWorldDestroy>();
+
+		world.component<CWorld>()
+			.on_remove([](CWorld& world)
+		{
+			world.types.world_type.DestroyPoly(world.world);
+		});
 	}
 }

src/Spatial3D/SpatialWorld.cpp
@@ -168,16 +168,20 @@ namespace voxel_game::spatial3d
 		}
 	}
 
-	void InitializeWorld(World& world)
+	World* CreateWorld(Types& types, uint8_t max_scale)
 	{
+		World* world = types.world_type.CreatePoly();
 
+		WorldSetMaxScale(types, *world, max_scale);
+
+		return world;
 	}
 
-	void ShutdownWorld(World& world)
+	void DestroyWorld(Types& types, World* world)
 	{
-		for (size_t scale_index = 0; scale_index < world.max_scale; scale_index++)
+		for (size_t scale_index = 0; scale_index < world->max_scale; scale_index++)
 		{
-			Scale& scale = GetScale(world, scale_index);
+			Scale& scale = GetScale(*world, scale_index);
 
 			DEBUG_ASSERT(scale.create_commands.empty(), "All commands should have been destroyed before destroying the world");
 			DEBUG_ASSERT(scale.load_commands.empty(), "All commands should have been destroyed before destroying the world");
@@ -185,6 +189,8 @@ namespace voxel_game::spatial3d
 			DEBUG_ASSERT(scale.destroy_commands.empty(), "All commands should have been destroyed before destroying the world");
 			DEBUG_ASSERT(scale.nodes.empty(), "All nodes should have been destroyed before destroying the world");
 		}
+
+		types.world_type.DestroyPoly(world);
 	}
 
 	void WorldSetMaxScale(Types& types, World& world, size_t max_scale)
@@ -230,7 +236,6 @@ namespace voxel_game::spatial3d
 		}
 
 		world.max_scale = max_scale;
-
 	}
 
 	void WorldCreateNodes(Types& types, World& world, const sim::CFrame& frame)

src/Spatial3D/SpatialWorld.h
@@ -105,7 +105,7 @@ namespace voxel_game::spatial3d
 	struct World : Nocopy, Nomove
 	{
 		godot::AABB bounds;
-		uint8_t max_scale = 1;
+		uint8_t max_scale = 0;
 		uint8_t node_size = 1;
 
 		Clock::duration node_keepalive = 10s;
@@ -140,9 +140,9 @@ namespace voxel_game::spatial3d
 
 	Node* GetNode(World& world, Coord coord);
 
-	void InitializeWorld(World& world);
+	World* CreateWorld(Types& types, uint8_t max_scale);
 
-	void ShutdownWorld(World& world);
+	void DestroyWorld(Types& types, World* world);
 
 	void WorldSetMaxScale(Types& types, World& world, size_t max_scale);
 

src/Universe/UniverseModule.cpp
@@ -16,6 +16,7 @@
 #include "Simulation/SimulationModule.h"
 
 #include "Render/RenderComponents.h"
+#include "Render/RenderModule.h"
 
 #include "Util/Debug.h"
 
@@ -35,10 +36,16 @@ namespace voxel_game::universe
 		universe_entity.emplace<sim::CPath>(path);
 		universe_entity.add<universe::CWorld>();
 		universe_entity.add<voxel::CWorld>(); // Testing
-		universe_entity.add<voxelrender::CWorld>(); // Testing
+		if (scenario_id.is_valid())
+		{
+			universe_entity.add<voxelrender::CWorld>(); // Testing
+		}
 
 		spatial3d::CWorld& spatial_world = universe_entity.ensure<spatial3d::CWorld>();
-		spatial_world.world->max_scale = spatial3d::k_max_world_scale;
+
+		DEBUG_ASSERT(!world.is_deferred(), "Observers need to be invoked to initialize poly types");
+
+		spatial_world.world = spatial3d::CreateWorld(spatial_world.types, spatial3d::k_max_world_scale);
 		spatial_world.world->node_size = 16;
 		spatial_world.world->node_keepalive = 1s;
 
@@ -51,6 +58,10 @@ namespace voxel_game::universe
 			scenario.id = scenario_id;
 
 			universe_entity.add<rendering::CTransform>();
+
+			spatial3d::Types& types = spatial_world.types;
+
+			WORLD_TO(spatial_world.world, voxelrender::World)->voxel_material = ALLOC_RENDER_RID(Material);
 		}
 
 		return universe_entity;

src/VoxelRender/VoxelRenderModule.cpp
@@ -126,8 +126,8 @@ namespace voxel_game::voxelrender
 
 		void LoadNode(spatial3d::Node* node)
 		{
-			NODE_TO(node, Node)->mesh = rendering::GetContext().allocator.GetRID(rendering::AllocateType::Mesh);
-			NODE_TO(node, Node)->mesh_instance = rendering::GetContext().allocator.GetRID(rendering::AllocateType::Instance);
+			NODE_TO(node, Node)->mesh = ALLOC_RENDER_RID(Mesh);
+			NODE_TO(node, Node)->mesh_instance = ALLOC_RENDER_RID(Instance);
 
 			ADD_RENDER_CMD(instance_set_base, NODE_TO(node, Node)->mesh_instance, NODE_TO(node, Node)->mesh);
 
@@ -168,25 +168,21 @@ namespace voxel_game::voxelrender
 		spatial3d::WorldType::RegisterType<World>();
 
 		// Initialise the spatial world of a universe
-		world.observer<spatial3d::CWorld, CWorld>(DEBUG_ONLY("InitializeVoxelRenderWorld"))
+		world.observer<spatial3d::CWorld>(DEBUG_ONLY("InitializeVoxelRenderWorld"))
 			.event(flecs::OnAdd)
-			.term_at(2).singleton().filter()
-			.each([](spatial3d::CWorld& spatial_world, CWorld& voxel_world)
+			.with<const CWorld>()
+			.each([](spatial3d::CWorld& spatial_world)
 		{
 			spatial_world.types.node_type.AddType<Node>();
 			spatial_world.types.scale_type.AddType<Scale>();
 			spatial_world.types.world_type.AddType<World>();
-
-			spatial3d::Types& types = spatial_world.types;
-
-			WORLD_TO(spatial_world.world, World)->voxel_material = rendering::GetContext().allocator.GetRID(rendering::AllocateType::Material);
 		});
 
 		// Initialise the spatial world of a universe
-		world.observer<spatial3d::CWorld, CWorld>(DEBUG_ONLY("UninitializeVoxelRenderWorld"))
+		world.observer<spatial3d::CWorld>(DEBUG_ONLY("UninitializeVoxelRenderWorld"))
 			.event(flecs::OnRemove)
-			.term_at(1).singleton().filter()
-			.each([](spatial3d::CWorld& spatial_world, CWorld& voxel_world)
+			.with<const CWorld>()
+			.each([](spatial3d::CWorld& spatial_world)
 		{
 			spatial3d::Types& types = spatial_world.types;
 
@@ -195,7 +191,7 @@ namespace voxel_game::voxelrender
 
 		world.system<spatial3d::CScale, const spatial3d::CWorld, voxel::CContext>(DEBUG_ONLY("VoxelRenderNodeModify"))
 			.multi_threaded()
-			.term_at(3).singleton()
+			.term_at(2).singleton()
 			.with<const voxel::CWorld>()
 			.with<const CWorld>()
 			.each([](flecs::entity entity, spatial3d::CScale& spatial_scale, const spatial3d::CWorld& spatial_world, voxel::CContext& voxel_ctx)