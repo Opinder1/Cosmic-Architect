212a368a47df3a315f46f157c76ac5e00534db95
Opinder <opinder.13@gmail.com>
2025-03-22 16:45:47 +0000

    Build the editor by default

--- buildenginevsproj.bat
+++ buildenginevsproj.bat
@@ -1,3 +1,3 @@
 cd C:/Godot/godotengine
 
-scons platform=windows target=editor dev_build=yes precision=double use_static_cpp=yes vsproj=yes bits=64 custom_modules=../Modules
+scons platform=windows dev_build=yes precision=double use_static_cpp=yes vsproj=yes bits=64 custom_modules=../Modules
